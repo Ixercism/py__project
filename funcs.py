3c05965c8ec9cc327d5784d1872c45529deea072
NASTJA <anastasiaizotova8010@gmail.com>
2024-01-23 17:20:07 +0300

    summation2

--- funcs.py
+++ funcs.py
@@ -1,10 +1,3 @@
-def summation():
-    a = int(input())
-    b = int(input())
-    print(a + b)
-summation()
-
-
 def subtraction():
     a = int(input())
     b = int(input())
---
+++ main.py
@@ -0,0 +1,5 @@
+def summation():
+    a = int(input())
+    b = int(input())
+    print(a + b)
+summation()
